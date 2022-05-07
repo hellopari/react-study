49c902328fba1aa48bd099c94b20f33558e06fa7
jiaojiao <zjj372517@alibaba-inc.com>
2022-05-07 15:41:26 +0800

    feat:1.0 version

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import './App.css';
+const list = [
+  {
+  title: 'React',
+  url: 'https://facebook.github.io/react/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://github.com/reactjs/redux',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+  ];
+
+  function isSearched(searchTerm) {
+    return function(item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+    }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+      searchTerm: '',
+      };
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item=> item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+    }
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input
+          type="text"
+          onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.filter(isSearched((this.state.searchTerm))).map(item =>{
+          const onHandleDismiss = () =>
+          this.onDismiss(item.objectID);
+        return(
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={onHandleDismiss}
+                type="button"
+                >
+                Dismiss
+              </button>
+            </span>
+          </div>)}
+        )}
+      </div>
+    );
+    }
+
+  }
+export default App;
